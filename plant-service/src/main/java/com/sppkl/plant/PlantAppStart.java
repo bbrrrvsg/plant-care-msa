f566e2a1d27b8e69c7a47c5bb09907618784b90e
ys06o <yongseongg00@gmail.com>
2026-03-09 18:09:45 +0900

    프로젝트 구조 수정본

--- plant-service/src/main/java/com/sppkl/plant/PlantAppStart.java
+++ plant-service/src/main/java/com/sppkl/plant/PlantAppStart.java
@@ -2,12 +2,10 @@ package com.sppkl.plant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class PlantAppStart {
     public static void main(String[] args) {
-        SpringApplication.run(PlantAppStart.class);
-
+        SpringApplication.run(PlantAppStart.class, args);
     }
 }
